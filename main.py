df2b6d9476f6c6b2c93c84123e8b87d5fb62bf69
l1nt3x <kianixx@mail.ru>
2021-10-14 20:54:42 +0300

    Update

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pickle
 import sys
+import webbrowser
 
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMenu, QVBoxLayout
-
-sys.path.insert(1, 'C:/Users/l1nt3x/PycharmProjects/compilation-of-python-libs-docs/ui')
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMenu, QVBoxLayout, QInputDialog, QDialog, \
+    QPushButton, QLabel
 
 from mainui import Ui_MainWindow
 
@@ -31,14 +30,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         contextMenu = QMenu(self)
         deleteAct = ''
         openAct = ''
-        newAct = ''
+        editAct = ''
         check = 'xd'
+        changeLinkAct = ''
+
         if ''.join([item.text() for item in self.listWidget.selectedItems()]) == '':
             newAct = contextMenu.addAction("New")
             check = 'new'
         else:
             openAct = contextMenu.addAction("Open")
             deleteAct = contextMenu.addAction("Delete")
+            editAct = contextMenu.addAction('Edit')
+            changeLinkAct = contextMenu.addAction('Change lib link')
             check = 'opedel'
         action = contextMenu.exec_(self.mapToGlobal(event.pos()))
         if check == 'opedel':
@@ -50,10 +53,45 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 with open('data.pickle', 'wb') as f:
                     pickle.dump(self.libsData, f)
             elif action == openAct:
-                web = QWebEngineView()
-                web.Load(QUrl(self.libsData[tmp[0]]))
-                # web.setWindowTitle(self.libsData[tmp[0]])
-                web.show()
+                if self.libsData[tmp[0]] == 'No link':
+                    dlg = QDialog()
+                    dlg.setFixedSize(160, 70)
+                    b1 = QPushButton("OK", dlg)
+                    b1.move(40, 40)
+                    b1.clicked.connect(dlg.close)
+                    l1 = QLabel('Please change link for this lib', dlg)
+                    l1.move(10, 10)
+                    dlg.setWindowTitle("No link")
+                    dlg.setWindowModality(Qt.ApplicationModal)
+                    dlg.exec_()
+                else:
+                    webbrowser.open(self.libsData[tmp[0]])
+
+            elif action == changeLinkAct:
+                edit_link, ok_pressed = QInputDialog.getText(self, "Change link",
+                                                             "Input lib link:")
+                if ok_pressed:
+                    self.libsData[tmp[0]] = edit_link
+                    with open('data.pickle', 'wb') as f:
+                        pickle.dump(self.libsData, f)
+            elif action == editAct:
+                edit_entry, ok_pressed = QInputDialog.getText(self, "Edit entry",
+                                                              "Input new lib name:")
+                if ok_pressed:
+                    del self.libsData[tmp[0]]
+                    self.libsData[edit_entry] = 'No link'
+                    self.listWidget.clear()
+                    self.listWidget.addItems(list(self.libsData.keys()))
+                    with open('data.pickle', 'wb') as f:
+                        pickle.dump(self.libsData, f)
+        elif check == 'new':
+            new_entry, ok_pressed = QInputDialog.getText(self, "New entry", "Input lib name:")
+            if ok_pressed:
+                self.libsData[new_entry] = 'No link'
+                self.listWidget.clear()
+                self.listWidget.addItems(list(self.libsData.keys()))
+                with open('data.pickle', 'wb') as f:
+                    pickle.dump(self.libsData, f)
         self.listWidget.clearSelection()
 
 
